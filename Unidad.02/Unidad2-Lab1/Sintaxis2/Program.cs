0d98ce5303086082a610f66b3bf32c9bfe3ffdb9
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 20:48:53 -0300

    Laboratorios de la unidad 2 hasta estructura de decision.

---
+++ Unidad.02/Unidad2-Lab1/Sintaxis2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese texto:");
+            string inputTexto = Console.ReadLine();
+
+            if (inputTexto == String.Empty)
+            {
+                Console.WriteLine("String vacio");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("1-Mayusculas, 2-Minusculas, 3-Longitud");
+
+                ConsoleKeyInfo opcion = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (opcion.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine(inputTexto.ToUpper());
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine(inputTexto.ToLower());
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine(inputTexto.Length);
+                        break;
+                    default: Console.WriteLine("Error de ingreso.");
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Unidad.02/Unidad2-Lab1/Unidad2-Lab1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese texto:");
+            string inputTexto = Console.ReadLine();
+            if (inputTexto == String.Empty)
+            {
+                Console.WriteLine("String vacio");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("1-Mayusculas, 2-Minusculas, 3-Longitud");
+
+                ConsoleKeyInfo opcion;
+                opcion = Console.ReadKey();
+                Console.WriteLine();
+
+                if (opcion.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine(inputTexto.ToUpper());
+                }
+                else if (opcion.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine(inputTexto.ToLower());
+                }
+                else if (opcion.Key == ConsoleKey.D3)
+                {
+                    Console.WriteLine(inputTexto.Length);
+                }
+                else
+                {
+                    Console.WriteLine("Error de ingreso.");
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
