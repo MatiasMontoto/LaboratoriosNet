78bed9a8d049d4047e8c39ab3390d1ba002fc43b
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 19:34:29 -0300

    Laboratorios de la unidad 1 de .NET

---
+++ Unidad.01/Unidad1-Capitulo2-Lab01/Consola/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteALine("Esta lina va a dar error!");
+        }
+    }
+}
