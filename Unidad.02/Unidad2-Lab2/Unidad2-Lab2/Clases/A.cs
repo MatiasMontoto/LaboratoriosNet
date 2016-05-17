ace28ffcea9f4e40f1bc7748a910058b8fa89d45
Matias <montoto.matias@yahoo.com.ar>
2016-05-17 00:37:45 -0300

    Se agrego el laboratorio de clases y herencia

---
+++ Unidad.02/Unidad2-Lab2/Unidad2-Lab2/Clases/A.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class A
+    {
+        public string NombreInstancia { get; set; }
+
+        public A()
+        {
+            NombreInstancia = "Instancia sin nombre";
+        }
+        public A(string nombre)
+        {
+            NombreInstancia = nombre;
+        }
+
+        public void MostrarNombre()
+        {
+            Console.WriteLine(NombreInstancia);
+        }
+
+        public void M1()
+        {
+            Console.WriteLine("Se invoco el metodo 1");
+        }
+        public void M2()
+        {
+            Console.WriteLine("Se invoco el metodo 2");
+        }
+        public void M3()
+        {
+            Console.WriteLine("Se invoco el metodo 3");
+        }
+    }
+}
---
+++ Unidad.02/Unidad2-Lab2/Unidad2-Lab2/LabClases1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Clases;
+
+namespace LabClases1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj1 = new A("Inst");
+            B obj2 = new B();
+
+            obj2.M1();
+            obj2.M2();
+            obj2.M3();
+            obj2.M4();
+            obj2.MostrarNombre();
+
+            obj1.MostrarNombre();
+            obj1.M1();
+            obj1.M2();
+            obj1.M3();
+
+            Console.ReadKey();
+        }
+    }
+}
