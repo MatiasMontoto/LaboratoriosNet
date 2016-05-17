8ed32cc617cfbf5c7137510bd08bd0eec3baad74
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 21:06:55 -0300

    Se agrego la parte 3 estructuras de iteracion.

--- Unidad.02/Unidad2-Lab1/Sintaxis3/Program.cs
+++ Unidad.02/Unidad2-Lab1/Sintaxis3/Program.cs
@@ -10,7 +10,23 @@ namespace Sintaxis3
     {
         static void Main(string[] args)
         {
+            int cantIteraciones = 5;
+            string[] arreglo = new string[cantIteraciones];
 
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                Console.WriteLine("Ingrese cadena {0}:",i);
+                arreglo[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = cantIteraciones-1; i > -1; i--)
+            {
+                Console.WriteLine("Cadena {0}: {1}", i, arreglo[i]);
+            }
+
+            Console.ReadKey();
         }
     }
 }
