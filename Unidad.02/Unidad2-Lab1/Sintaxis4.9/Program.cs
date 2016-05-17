9e76527bd1b397128e2b914b522c7fa32e523b5f
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 23:52:48 -0300

    Se agrego el punto 4.9 del laboratorio

---
+++ Unidad.02/Unidad2-Lab1/Sintaxis4.9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis4._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el numero de filas:");
+            int num = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < num; i++)
+            {
+                for (int b = 0; b < num - (i + 1); b++)
+                    Console.Write(" ");
+
+                for (int j = -2; j < (2 * i) - 1; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
