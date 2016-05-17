bda33ddda06729b377f8ce8843584832d2033870
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 21:22:17 -0300

    Se agrego el punto 4.1 del laboratorio.

---
+++ Unidad.02/Unidad2-Lab1/Sintaxis4.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numero1, numero2;
+            int a = 0, b = 0, c = 0;
+
+            Console.Write("Ingrese primer numero: ");
+            numero1 = Console.ReadLine();
+            Console.Write("Ingrese segundo numero: ");
+            numero2 = Console.ReadLine();
+
+            a = Int32.Parse(numero1);
+            b = Int32.Parse(numero2);
+            c = a + b;
+
+            Console.WriteLine("El resultado de la suma de: " + numero1 + " y " + numero2 + " es: " + c);
+            Console.ReadKey();
+        }
+    }
+}
