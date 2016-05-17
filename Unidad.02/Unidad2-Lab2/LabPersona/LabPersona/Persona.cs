d4205f1d0f93df506ff1c2c3bd6a0e6681ecdcf3
Matias <montoto.matias@yahoo.com.ar>
2016-05-17 14:22:22 -0300

    Se agrego el laboratorio Clase Perosna

---
+++ Unidad.02/Unidad2-Lab2/LabPersona/LabPersona/Persona.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabPersona
+{
+    public class Persona
+    {
+        private string _Nombre;
+        private string _Apellido;
+        private string _Edad;
+        private string _Dni;
+
+        public Persona(string nombre, string apellido, string edad, string dni)
+        {
+            _Nombre = nombre;
+            _Apellido = apellido;
+            _Edad = edad;
+            _Dni = dni;
+            Console.WriteLine("se ha creado una instancia de persona");
+        }
+
+        ~Persona()
+        {
+            Console.WriteLine("se ha eliminado una instancia de persona");
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                return _Apellido;
+            }
+
+            set
+            {
+                _Apellido = value;
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _Nombre;
+            }
+
+            set
+            {
+                _Nombre = value;
+            }
+        }
+
+        public string Dni
+        {
+            get
+            {
+                return _Dni;
+            }
+
+            set
+            {
+                _Dni = value;
+            }
+        }
+
+        public string Edad
+        {
+            get
+            {
+                return _Edad;
+            }
+
+            set
+            {
+                _Edad = value;
+            }
+        }
+
+        public void GetFullName()
+        {
+            Console.WriteLine(_Nombre + _Apellido);
+        }
+
+        public void GetAge()
+        {
+            Console.WriteLine(_Edad);
+        }
+    }
+}
