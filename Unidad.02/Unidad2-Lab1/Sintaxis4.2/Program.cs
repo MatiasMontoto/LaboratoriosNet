24e4ccfa2419bfe74f5ec4e7b9c449586bafe9cf
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 21:42:12 -0300

    Se agrego el punto 4.2 del laboratorio.

---
+++ Unidad.02/Unidad2-Lab1/Sintaxis4.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis4._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string año;
+            int a;
+            Console.Write("Ingrese un año: ");
+            año = Console.ReadLine();
+
+            a = Int32.Parse(año);
+
+            if ( (a%4) == 0)
+            {
+                if ( (a%100) == 0)
+                {
+                    if ( (a%400) == 0)
+                    {
+                        Console.WriteLine("Año bisiesto");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Año no bisiesto");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Año bisiesto");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Año no bisiesto");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
