0c99048b6c5715983b3bd8b54836b47ea95feae0
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 23:45:20 -0300

    Se agrego el punto 4.8 del laboratorio

---
+++ Unidad.02/Unidad2-Lab1/Sintaxis4.8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis4._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 0;
+            string clave, pass = "asdfg";
+            while (intentos < 4)
+            {
+                intentos++;
+                Console.WriteLine("Ingrese contraseña:");
+                clave = Console.ReadLine();
+                if (clave == pass)
+                {
+                    Console.WriteLine("Contraseña ingresada correctamente");
+                    intentos = 4;
+                }
+                else
+                {
+                    Console.WriteLine("Contraseña incorrecta");
+                    Console.WriteLine("Intento {0} de 4", intentos);
+                }
+            }
+            Console.WriteLine("Presione una tecla para continuar");
+            Console.ReadKey();
+        }
+    }
+}
