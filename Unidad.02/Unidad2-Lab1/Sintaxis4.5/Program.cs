5dd4fec97df5ef83816ea7d7e167888dc764908a
Matias <montoto.matias@yahoo.com.ar>
2016-05-16 22:35:01 -0300

    Se agrego el punto 4.5 del laboratorio

---
+++ Unidad.02/Unidad2-Lab1/Sintaxis4.5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sintaxis4._5
+{
+    class Program
+    {
+        enum Meses
+        {
+            enero=1, febrero, marzo, abril, mayo, junio, julio, agosto, septiembre, octubre, noviembre, diciembre
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese mes: ");
+            string mes = Console.ReadLine();
+            
+            for (int i = 0; i < 12; i++)
+            {
+                if (Enum.GetName(typeof(Meses), i) == mes)
+                {
+                    Console.WriteLine(mes + " es el mes numero: " + (int)i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
